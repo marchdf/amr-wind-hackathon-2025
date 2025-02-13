41d71f477a650f2d9fb64a2bba48aa85f907fd97
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2025-02-13 11:30:18 -0700

    updates

--- profiling/kestrel-hpctoolkit.sh
+++ profiling/kestrel-hpctoolkit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -J profiling
+#SBATCH -J profiling-hpctoolkit
 #SBATCH -o %x.o%j
 #SBATCH --account=flowmas   # Required
-#SBATCH --time=01:00:00
+#SBATCH --time=00:05:00
 #SBATCH --nodes=8
 #SBATCH --ntasks-per-node=128
 #SBATCH --gpus=32
@@ -22,15 +22,17 @@ cmd "export MPICH_GPU_SUPPORT_ENABLED=1"
 cmd "export EXAWIND_MANAGER=${HOME}/exawind/exawind-manager"
 cmd "source ${EXAWIND_MANAGER}/start.sh && spack-start"
 cmd "spack env activate -d ${EXAWIND_MANAGER}/environments/amr-wind-cuda"
-cmd "spack load amr-wind+cuda build_type=Release"
-# cmd "spack load amr-wind+cuda build_type=RelWithDebInfo"
+# cmd "spack load amr-wind+cuda build_type=Release"
+cmd "spack load amr-wind+cuda build_type=RelWithDebInfo"
 cmd "spack load hpctoolkit"
 cmd "export MPICH_OFI_SKIP_NIC_SYMMETRY_TEST=1"
 cmd "export MPICH_GPU_SUPPORT_ENABLED=1"
 cmd "which amr_wind"
 cmd "which hpcrun"
 PROFNAME="profiling"
+cmd "rm -rf ${PROFNAME}-hpctoolkit"
+cmd "rm -rf ${PROFNAME}-hpctoolkit_db"
 # cmd "srun -N ${SLURM_JOB_NUM_NODES} -n ${RANKS} --ntasks-per-node=${NTASKS_PER_NODE} --gpus-per-node=4 --gpu-bind=closest amr_wind demo_case.inp amrex.abort_on_out_of_gpu_memory=1 amrex.the_arena_is_managed=0 amr.blocking_factor=16 amr.max_grid_size=128 amrex.use_profiler_syncs=0 amrex.async_out=0 amrex.use_gpu_aware_mpi=1 time.max_step=40040 > out.log"
-cmd "srun -N ${SLURM_JOB_NUM_NODES} -n ${RANKS} --ntasks-per-node=${NTASKS_PER_NODE} --gpus-per-node=4 --gpu-bind=closest hpcrun -o ${PROFNAME} -f 0.2 -t -e CPUTIME -e gpu=nvidia amr_wind demo_case.inp amrex.abort_on_out_of_gpu_memory=1 amrex.the_arena_is_managed=0 amr.blocking_factor=16 amr.max_grid_size=128 amrex.use_profiler_syncs=0 amrex.async_out=0 amrex.use_gpu_aware_mpi=1 time.max_step=40040 > out.log"
+cmd "srun -N ${SLURM_JOB_NUM_NODES} -n ${RANKS} --ntasks-per-node=${NTASKS_PER_NODE} --gpus-per-node=4 --gpu-bind=closest hpcrun -o ${PROFNAME} -f 0.2 -t -e CPUTIME -e gpu=nvidia amr_wind demo_case.inp amrex.abort_on_out_of_gpu_memory=1 amrex.the_arena_is_managed=0 amr.blocking_factor=16 amr.max_grid_size=128 amrex.use_profiler_syncs=0 amrex.async_out=0 amrex.use_gpu_aware_mpi=1 time.max_step=40040 > out-hpctoolkit.log"
 cmd "hpcstruct ${PROFNAME}"
 cmd "hpcprof -o ${PROFNAME}_db ${PROFNAME}"
