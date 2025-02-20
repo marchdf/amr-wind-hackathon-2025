2538ed913b53b31f6bf37046526a6aebb342eb32
Prakash Mohan <Prakash.Mohan@nrel.gov>
2025-02-20 16:22:31 -0700

    minor output tweak

--- profiling/kestrel-nruns.sh
+++ profiling/kestrel-nruns.sh
@@ -32,4 +32,4 @@ for i in {1..5}; do
     cmd "srun -N ${SLURM_JOB_NUM_NODES} -n ${RANKS} --ntasks-per-node=${NTASKS_PER_NODE} --gpus-per-node=4 --gpu-bind=closest amr_wind demo_case.inp amrex.abort_on_out_of_gpu_memory=1 amrex.the_arena_is_managed=0 amr.blocking_factor=16 amr.max_grid_size=128 amrex.use_profiler_syncs=0 amrex.async_out=0 amrex.use_gpu_aware_mpi=1 time.max_step=40040 > out-${SLURM_JOB_ID}-run${i}.log"
     cmd "sleep 1s"
 done
-grep "Time spent in Evolve" out-${SLURM_JOB_ID}*.log | cut -f 3 -d ':' | tr -s ' ' | awk '{tot+=$1; sq+=$1*$1} END {print "average Evolve() time: " tot/NR "std: " (sq/NR - (tot/NR)**2)**0.5}'
+grep "Time spent in Evolve" out-${SLURM_JOB_ID}*.log | cut -f 3 -d ':' | tr -s ' ' | awk '{tot+=$1; sq+=$1*$1} END {print "Evolve() times mean: " tot/NR " std: " (sq/NR - (tot/NR)**2)**0.5}'
