a0db7074e799ed47bd099202bc48dd9770f3b875
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2025-02-20 14:18:34 -0700

    minor edits

--- profiling/kestrel-hpctoolkit.sh
+++ profiling/kestrel-hpctoolkit.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH -J profiling-hpctoolkit
 #SBATCH -o %x.o%j
-#SBATCH --account hackathon
-#SBATCH --reservation hackathon
+#SBATCH --account flowmas
+# #SBATCH --account hackathon
+# #SBATCH --reservation hackathon
 #SBATCH --time=00:20:00
 #SBATCH --nodes=8
 #SBATCH --ntasks-per-node=128
@@ -34,7 +35,6 @@ cmd "which hpcrun"
 PROFNAME="profiling"
 cmd "rm -rf ${PROFNAME}"
 cmd "rm -rf ${PROFNAME}_db"
-# cmd "srun -N ${SLURM_JOB_NUM_NODES} -n ${RANKS} --ntasks-per-node=${NTASKS_PER_NODE} --gpus-per-node=4 --gpu-bind=closest amr_wind demo_case.inp amrex.abort_on_out_of_gpu_memory=1 amrex.the_arena_is_managed=0 amr.blocking_factor=16 amr.max_grid_size=128 amrex.use_profiler_syncs=0 amrex.async_out=0 amrex.use_gpu_aware_mpi=1 time.max_step=40040 > out.log"
 cmd "srun -N ${SLURM_JOB_NUM_NODES} -n ${RANKS} --ntasks-per-node=${NTASKS_PER_NODE} --gpus-per-node=4 --gpu-bind=closest hpcrun -o ${PROFNAME} -tt -e CPUTIME -e gpu=nvidia amr_wind demo_case.inp amrex.abort_on_out_of_gpu_memory=1 amrex.the_arena_is_managed=0 amr.blocking_factor=16 amr.max_grid_size=128 amrex.use_profiler_syncs=0 amrex.async_out=0 amrex.use_gpu_aware_mpi=1 time.max_step=40005 > out-hpctoolkit-${SLURM_JOB_ID}.log"
 cmd "hpcstruct ${PROFNAME}"
 cmd "hpcprof -o ${PROFNAME}_db ${PROFNAME}"
--- profiling/kestrel-nsys.sh
+++ profiling/kestrel-nsys.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH -J profiling-nsys
 #SBATCH -o %x.o%j
-#SBATCH --account hackathon
-#SBATCH --reservation hackathon
+#SBATCH --account flowmas
+# #SBATCH --account hackathon
+# #SBATCH --reservation hackathon
 #SBATCH --time=00:10:00
 #SBATCH --nodes=8
 #SBATCH --ntasks-per-node=128
@@ -31,5 +32,4 @@ cmd "export MPICH_GPU_SUPPORT_ENABLED=1"
 cmd "which amr_wind"
 cmd "which nsys"
 cmd "rm -f report*.sqlite report*.qdstrm report*.nsys-rep"
-# cmd "srun -N ${SLURM_JOB_NUM_NODES} -n ${RANKS} --ntasks-per-node=${NTASKS_PER_NODE} --gpus-per-node=4 --gpu-bind=closest nsys profile --stats=true --trace=nvtx,cuda,mpi --mpi-impl=mpich amr_wind demo_case.inp amrex.abort_on_out_of_gpu_memory=1 amrex.the_arena_is_managed=0 amr.blocking_factor=16 amr.max_grid_size=128 amrex.use_profiler_syncs=0 amrex.async_out=0 amrex.use_gpu_aware_mpi=1 time.max_step=40040 > out-nsys-${SLURM_JOB_ID}.log"
 cmd "srun -N ${SLURM_JOB_NUM_NODES} -n ${RANKS} --ntasks-per-node=${NTASKS_PER_NODE} --gpus-per-node=4 --gpu-bind=closest nsys profile --stats=true --trace=nvtx,cuda,mpi --mpi-impl=mpich amr_wind demo_case.inp amrex.abort_on_out_of_gpu_memory=1 amrex.the_arena_is_managed=0 amr.blocking_factor=16 amr.max_grid_size=128 amrex.use_profiler_syncs=0 amrex.async_out=0 amrex.use_gpu_aware_mpi=1 time.max_step=40005 mac_proj.verbose=1 mac_proj.bottom_verbose=1 nodal_proj.verbose=1 nodal_proj.bottom_verbose=1 > out-nsys-${SLURM_JOB_ID}.log"
